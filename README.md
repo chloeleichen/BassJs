92715f09841503897212864d4d52c17719c4b49e
Chloe Chen <chloeleichen@gmail.com>
2015-04-27 15:31:34 +1000

    added js clock

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # BassJs
 A vanilla JS UI library for the lightning-fast, modular CSS framework basscss: http://www.basscss.com/
+Written in Object Oriented Javascript 
--- src/script.js
+++ src/script.js
@@ -1,21 +1,40 @@
-var Clock  = function(target, trigger){
+var Clock  = function(target, trigger, stop){
 	this.target = target;
 	this.trigger = trigger;
+	this.stop = stop;
+	this ._init();
 }
 
 Clock.prototype = {
 	_create:  function(){
 			var date = new Date();
-			var hour = date.getHours() < 10? 0 + date.getHours : date.getHours();
-			var min = date.getMinutes() < 10? 0 + date.getMinutes : date.getMinutes();
-			var sec = date.getSeconds() < 10? 0 + date.getSeconds : date.getSeconds();
+			var hour = date.getHours() < 10? 0 + String(date.getHours()) : String(date.getHours());
+			var min = date.getMinutes() < 10? 0 + String(date.getMinutes()) : String(date.getMinutes());
+			var sec = date.getSeconds() < 10? 0 + String(date.getSeconds()) : String(date.getSeconds());
 			var currentTime = hour + ":" + min + ":" + sec ;
 			this.target.innerHTML = currentTime;
 	}
+	,
+	_update:  function(){
+		var self = this;
+		var start = setTimeout(function(){
+			self._create();
+			self._update();
+			}, 1000);
+	
 
-	_start:  function(){
-			
+		self.stop.addEventListener("click", function(){
+		clearTimeout(start);
+		});
+	}
+	,
+	_init: function(){
+		var self = this;
+		//self._create();
+		self.trigger.addEventListener("click", function(){
+			self._update();
+		});
 	}
 }
 
-var c = new Clock(document.getElementById("clock"));
+var c = new Clock(document.getElementById("clock"), document.getElementById("start"), document.getElementById("stop"));
