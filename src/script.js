da5cd60d130198a7ce2969537b261d1451884526
Chloe Chen <chloeleichen@gmail.com>
2015-04-28 20:19:23 +1000

    add progress animation

--- src/script.js
+++ src/script.js
@@ -4,7 +4,6 @@ var Clock  = function(target, trigger){
 	this.stop = stop;
 	this ._init();
 }
-
 Clock.prototype = {
 	_create:  function(){
 		var date = new Date();
@@ -21,12 +20,6 @@ Clock.prototype = {
 			self.target.style.color = "black";
 			self._create();
 		}, 1000);
-	}
-	,
-
-	_reset: function(){
-
-
 	}
 	,
 	_init: function(){
@@ -47,9 +40,8 @@ Clock.prototype = {
 		}
 		
 	});
+  }
 }
-}
-
 //var c = new Clock(document.getElementById("clock"), document.getElementById("clock-control"));
 
 // Animation, the old way, not request animation frame 
@@ -60,11 +52,13 @@ Clock.prototype = {
 //		  progress: the fraction of animation time that has already passed, calculated on every frame as timePassed/duration, [0, 1]
 //        delta(progess) a function that returns the current animation progress  
 
+var Animation = (function(){
+
 	var animate =  function(options){
 		var start = new Date();
 		var id = setInterval(function(){
-		var timePassed = new Date() - start;
-		var progress = timePassed/options.duration;
+			var timePassed = new Date() - start;
+			var progress = timePassed/options.duration;
 		//Call this first, make progess = 1, avoid Math.acos(progess) NaN error 
 		if(progress > 1) progress = 1; 
 		var delta = options.delta(progress);
@@ -73,30 +67,32 @@ Clock.prototype = {
 			clearInterval(id);			
 		}
 	}, options.delay || 10)
-}
+	}
 	
-	var move = function(element, trigger, delta, duration){
+	var move = function(element, trigger, options){
 		trigger.addEventListener("click", function(){
 			animate({
-			delay: 10,
-			duration: duration || 1000,
-			delta: delta,
-			step: function(delta){
-				element.value = delta;
-			}
+				delay: 10,
+				duration: options.duration || 1000,
+				delta: options.delta,
+				step: function(delta){
+					element.value = delta;
+				}
 
-		}) 
+			}) 
 		});
 		
 	}
+	return(move);
+})();
 
-
+var getFunc = function(func) {
 
 	var deltaLiner = function(p){return p}
 	//The Math.pow(base, exponent) function returns the base to the exponent power, that is, baseexponent.
 
 	var deltaPow = function(p){
-		return Math.pow(p, 5);
+		return Math.pow(p, 2);
 	}
 
 	//The Math.acos() function returns the arccosine (in radians) of a number
@@ -108,57 +104,71 @@ Clock.prototype = {
 		x = 1.5;
 		return Math.pow(p, 2) * ((x + 1) * p - x);
 	}
-
 	var deltaElastic = function(p, x){
 		x = 1.5;
 		return Math.pow(2, 10 * (p-1)) * Math.cos(20*Math.PI*x/3*p)
 	}
-
 	// bounce 
 	function deltaBounce(progress){
 		for(var a = 0, b = 1, result; 1; a += b, b /= 2) {
-	    if (progress >= (7 - 4 * a) / 11) {
-	      return -Math.pow((11 - 6 * a - 11 * progress) / 4, 2) + Math.pow(b, 2);
-	    }
-	  }
-	
-	}
+			if (progress >= (7 - 4 * a) / 11) {
+				return -Math.pow((11 - 6 * a - 11 * progress) / 4, 2) + Math.pow(b, 2);
+			}
+		}
 
+	}
 	function makeEaseOut(delta) { 
-	  return function(progress) {
-	    return 1 - delta(1 - progress);
-	  }
+		return function(progress) {
+			return 1 - delta(1 - progress);
+		}
 	}
 	function makeEaseIn(delta) { 
-	  return function(progress) {
-	    return delta(progress);
-	  }
+		return function(progress) {
+			return delta(progress);
+		}
 	}
-
 	function makeEaseInOut(delta) { 
 		return function(progress){
 			if (progress <= 0.5) { // the first half of the animation)
-			  return delta(2 * progress) / 2
+	return delta(2 * progress) / 2
 			} else { // the second half
-			  return (2 - delta(2 * (1 - progress))) / 2
+				return (2 - delta(2 * (1 - progress))) / 2
 			}
-
 		}
 	}
-
 	var deltaBounceEaseOut = makeEaseOut(deltaBounce);
 	var deltaBounceEaseIn = makeEaseIn(deltaBounce);
 	var deltaBounceEaseInOut = makeEaseInOut(deltaBounce);
 
+	switch(func){
+		case "liner": 
+		return deltaLiner;
+		case "pow": 
+		return deltaPow;
+		case "bow": 
+		return deltaBow;
+		case "circ":
+		return deltaCirc;
+		case "elastic":
+		return deltaElastic;
+		case "bounce":
+		return deltaBounce;
+		case "bounce ease out":
+		return deltaBounceEaseOut;
+		case "bounce ease in":
+		return deltaBounceEaseIn;
+		case "bounce ease in out":
+		return deltaBounceEaseInOut;
+	}
+}
 
+var progressLiner = new Animation(document.getElementById("progress-liner"), document.getElementById("block-control-liner"), {delta: getFunc("liner"), duration: 4000});
 
-	//move(document.getElementById("progress-liner"), document.getElementById("block-control-liner"),  deltaLiner, 2000);
-	move(document.getElementById("progress-pow"), document.getElementById("block-control-pow"),  deltaPow, 4000);
-	// move(document.getElementById("progress-bow"), document.getElementById("block-control-bow"),  deltaBow, 2000);
-	// move(document.getElementById("progress-ease-out"), document.getElementById("block-control-ease-out"),  deltaBounceEaseOut, 2000);
+var progressPow = new Animation(document.getElementById("progress-pow"), document.getElementById("block-control-pow"), {delta: getFunc("pow"), duration: 4000});
 
+var progressPow = new Animation(document.getElementById("progress-bounce"), document.getElementById("block-control-bounce"), {delta: getFunc("bounce"), duration: 4000});
 
-
+var progressIn = new Animation(document.getElementById("progress-ease-out"), document.getElementById("block-control-ease-out"), {delta: getFunc("bounce ease out"), duration: 4000});
 
 
 
